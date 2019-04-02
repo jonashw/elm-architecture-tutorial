31797dc3c1eddd920deb3e26726d109520504b19
Jon Wilson <2jonwilson@gmail.com>
2019-04-02 10:40:00 -0700

    01-button: added a count of presses and a reset button

--- examples/01-button.elm
+++ examples/01-button.elm
@@ -1,5 +1,6 @@
 import Browser
-import Html exposing (Html, button, div, text)
+import Html exposing (Html, button, div, text, span)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
 
 
@@ -9,26 +10,24 @@ main =
 
 -- MODEL
 
-type alias Model = Int
+type alias Model = {count: Int, presses: Int}
 
 init : Model
 init =
-  0
+  {count = 0, presses = 0}
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Reset
 
 update : Msg -> Model -> Model
 update msg model =
+  let presses = model.presses + 1 in
   case msg of
-    Increment ->
-      model + 1
-
-    Decrement ->
-      model - 1
-
+    Increment -> {count = model.count + 1, presses = presses }
+    Decrement -> {count = model.count - 1, presses = presses }
+    Reset     -> {count = 0              , presses = 0 }
 
 -- VIEW
 
@@ -36,6 +35,11 @@ view : Model -> Html Msg
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , span [style "padding" "1em"] 
+      [ span [] [ text ("Count: " ++ (String.fromInt model.count)) ]
+      , span [style "padding-left" "0.66em"] [ text ("Presses: " ++ (String.fromInt model.presses)) ]
+      ]
     , button [ onClick Increment ] [ text "+" ]
+    , div [style "padding-top" "1em"] 
+      [ button [ onClick Reset ] [ text "reset" ] ]
     ]
