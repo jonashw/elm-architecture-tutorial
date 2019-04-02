53dad9c6258adfdb4e83a88d6d507ed11afd0dfa
Jon Wilson <2jonwilson@gmail.com>
2019-04-02 10:42:06 -0700

    03-form: removed some querying from the view via 'Status' in the model

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -3,76 +3,74 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
-
-
 -- MAIN
-
-
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
-
-
 -- MODEL
-
-
+type Status = Empty | UnmatchedPasswords | OK
 type alias Model =
   { name : String
   , password : String
   , passwordAgain : String
+  , status: Status
   }
-
 
 init : Model
 init =
-  Model "" "" ""
-
-
+  Model "" "" "" Empty
 
 -- UPDATE
-
-
 type Msg
   = Name String
   | Password String
   | PasswordAgain String
 
-
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
-
-    Password password ->
-      { model | password = password }
-
-    PasswordAgain password ->
-      { model | passwordAgain = password }
-
-
+  let 
+    updated = 
+      case msg of
+        Name name              -> { model | name = name }
+        Password password      -> { model | password = password }
+        PasswordAgain password -> { model | passwordAgain = password }
+    status = 
+      case (updated.name, updated.password, updated.passwordAgain) of
+      ("",_,_) -> Empty
+      (_,"",_) -> Empty
+      (_,_,"") -> Empty
+      (_,p,pa) ->
+        if p == pa
+        then OK
+        else UnmatchedPasswords
+  in {updated | status = status}
 
 -- VIEW
 
-
 view : Model -> Html Msg
 view model =
-  div []
-    [ viewInput "text" "Name" model.name Name
-    , viewInput "password" "Password" model.password Password
-    , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-    , viewValidation model
+  Html.form []
+    [
+      div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewValidation model
+        ]
     ]
-
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
-  input [ type_ t, placeholder p, value v, onInput toMsg ] []
-
+  let 
+    extraAttr = 
+      if t == "password"
+      then [Html.Attributes.autocomplete False]
+      else []
+  in input ([ type_ t, placeholder p, value v, onInput toMsg ] ++ extraAttr) []
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if model.password == model.passwordAgain then
-    div [ style "color" "green" ] [ text "OK" ]
-  else
-    div [ style "color" "red" ] [ text "Passwords do not match!" ]
+  case model.status of
+  Empty              -> div [ style "color" "red" ] [ text "All fields required" ]
+  UnmatchedPasswords -> div [ style "color" "red" ] [ text "Passwords do not match!" ]
+  OK                 -> div [ style "color" "green" ] [ text "OK" ]
